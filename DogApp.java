74395756cb47fd1880c17a951b90ec35be2f4251
srividya0103 <17wh1a0579@bvrithyderabad.edu.in>
2021-04-08 17:27:24 +0530

    Create DogApp.java

---
+++ DogApp.java
@@ -0,0 +1,11 @@
+package day3.task1;
+
+public class DogApp {
+	public static void main(String[] args) {
+		Dog d1 = new Dog();
+		d1.setName("Tommy");
+		d1.setAge(8);
+		d1.setWeight(3);
+		d1.playGames();
+	}
+}
